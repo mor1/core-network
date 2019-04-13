e98daa4ec9fd538465efd9b397c87ce874c83587
Richard Mortier <mort@cantab.net>
2019-04-13 21:07:57 +0100

    dune: tweaks

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-
-#sudo apk add iptables iproute2 tcpdump curl net-tools
 
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -F INPUT
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT  -p udp --sport 53 -j ACCEPT
-iptables -A INPUT  -p tcp --sport 53 -j ACCEPT
-
+iptables -A INPUT -p udp --sport 53 -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -j ACCEPT
 
 echo starting core-network...
 ./core-network $@
