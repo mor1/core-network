45703bc20c375006bf969f745f05a9f3d927d687
Richard Mortier <mort@cantab.net>
2019-04-13 21:04:39 +0100

    dunification

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# compile code
-echo "compiling..."
-opam config exec -- jbuilder build bin/core_network.exe
-cp ./_build/default/bin/core_network.exe ./core-network
-echo "done compiling"
